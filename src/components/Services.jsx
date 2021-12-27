4c481d3b7b6ce7ff831c49e77a62ff2b090eb3ea
Tomáš <to.tichy@gmail.com>
2021-12-27 23:02:48 +0100

    Services completed

---
+++ src/components/Services.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faDesktop, faFileCode } from "@fortawesome/free-solid-svg-icons";
+
+const Services = () => {
+  return (
+    <div className='services'>
+      <h1 className='py-5'>My Services</h1>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-3 col-sm-6'>
+            <div className='box'>
+              <div className='circle'>
+                <FontAwesomeIcon className='icon' icon={faDesktop} size='2x' />
+              </div>
+              <h3>Web Design</h3>
+              <p>
+                I approach each project individually and alway on the result.
+              </p>
+            </div>
+          </div>
+          <div className='col-md-3 col-sm-6'>
+            <div className='box'>
+              <div className='circle'>
+                <FontAwesomeIcon className='icon' icon={faFileCode} size='2x' />
+              </div>
+              <h3>Web Development</h3>
+              <p>Your website will be build with a new proven technologies.</p>
+            </div>
+          </div>
+          <div className='col-md-3 col-sm-6'>
+            <div className='box'>
+              <div className='circle'>
+                <FontAwesomeIcon
+                  className='icon'
+                  icon={faFacebookF}
+                  size='2x'
+                />
+              </div>
+              <h3>Facebook Ads SMM</h3>
+              <p>
+                Your potential clients will see your srvices or products on
+                Facebook
+              </p>
+            </div>
+          </div>
+          <div className='col-md-3 col-sm-6'>
+            <div className='box'>
+              <div className='circle'>
+                <FontAwesomeIcon className='icon' icon={faGoogle} size='2x' />
+              </div>
+              <h3>Google Ads</h3>
+              <p>
+                Your service or product will appear at the top of the Google
+                search
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
